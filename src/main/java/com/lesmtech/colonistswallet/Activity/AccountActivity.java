c030ccf00108d92b3d5c8b00393bfb0b80eb0969
Te <sheat2500@gmail.com>
2015-01-26 00:05:58 -0500

    httppost request demo

--- src/main/java/com/lesmtech/colonistswallet/Activity/AccountActivity.java
+++ src/main/java/com/lesmtech/colonistswallet/Activity/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.lesmtech.colonistswallet.Activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.lesmtech.colonistswallet.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Te on 1/24/15.
@@ -34,6 +51,45 @@ public class AccountActivity extends FragmentActivity {
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View v = inflater.inflate(R.layout.fragment_register, container, false);
             return v;
+        }
+    }
+
+    // Background thread with HttpPost Request
+    private class LogInThread extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://www.bostonmiya.com/test/registration.do");
+            List<NameValuePair> nameValuePairs = new ArrayList<>();
+            nameValuePairs.add(new BasicNameValuePair("username", "rindt"));
+            nameValuePairs.add(new BasicNameValuePair("password", "12345"));
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    builder.append(line);
+                }
+                System.out.print(builder.toString());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "true";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+
         }
     }
 }
